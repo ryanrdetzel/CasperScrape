54ab19b2c8d49c2eaea6f2598aa8a8745f95dc52
Ryan Detzel <ryan.detzel@gmail.com>
2014-03-19 20:24:45 -0400

    Update purchase.js

--- purchase.js
+++ purchase.js
@@ -1,16 +1,16 @@
-var casper = require('casper').create({
-  verbose: false,
-  logLevel: "debug"
-});
+/* Proof of concept */
+var casper = require('casper').create();
 
 var snapshot = 0;
-function debugSnapshot(){
+function debugSnapshot(message){
+  /* Saves a screenshot of the page to see what the bot sees */
   casper.capture('jcrew' + (snapshot++) + '.png', {
         top: 0,
         left: 0,
         width: 1024,
         height: 2000
   });
+  casper.echo(message);
 }
 
 var url = 'https://www.jcrew.com/mens_category/polostees/brokeninjerseytees/PRD~72977/99102149736/72977.jsp?N=21+16&Nbrd=J&Nloc=en_US&Nrpp=48&Npge=1&Nsrt=3&isFromSale=true&isSaleItem=true&isNewSearch=true';
@@ -20,50 +20,36 @@ casper.start(url, function() {
 });
 
 casper.thenClick('a.add-item', function(){
-  this.echo("Clicked on add item");
-  debugSnapshot()
+  debugSnapshot("Clicked on add item")
 });
 
 casper.thenClick('#viewShoppingBagButton', function(){
-  this.echo("Clicking on cart");
-  debugSnapshot()
+  debugSnapshot("Clicking on cart")
 });
-
 
 /* Apply promotion */
 casper.thenClick('#hasPromo',function() {
-  this.echo("Has promo");
   this.fill('#summary-promo-form', {
     promotionCode: 'HEYSPRING'
   }, false);
-
-  console.log(this.getCurrentUrl()); 
   this.evaluate(function(){
-    document.querySelector('body').style.backgroundColor='blue';
-    __utils__.echo("HERE");
+    //__utils__.echo("executing js directly on the page");
     CheckOutPage.applyDiscount('promoApply');
   });
-
-  
 });
 
 casper.waitForText("off your purchase", function(){
-  this.echo("Wait for text");
+  this.echo("Wait for page to load");
 });
 
 casper.then(function(){
-  this.echo("Apply promo");
-  debugSnapshot();
+  debugSnapshot("Apply promo");
 });
-
 
 casper.thenClick('#button-checkout', function(){
-  console.log("Checkout");
-  debugSnapshot();
+  debugSnapshot("Checkout");
 });
 
-
-// Check the total privae to make sure it's correct
-// Fill out the form and you're done
-
+// TODO: Check the total price to make sure it's correct before we continue
+// TODO: Fill out the form with user information and submit.
 casper.run();
